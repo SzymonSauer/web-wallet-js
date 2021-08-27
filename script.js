4f64effce43128539860d61b77568d7607b572c5
Szymon Sauer <szymonsauer1@gmail.com>
2021-08-27 15:12:57 +0200

    changes

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const checkForm = () => {
 		amountInput.value !== '' &&
 		categorySelect.value !== 'none'
 	) {
-		console.log('ok');
+		createNewTransaction();
 	} else {
 		alert('Wypełnij wszystkie pola!');
 	}
@@ -46,6 +46,61 @@ const clearInputs = () => {
 	nameInput.value = '';
 	amountInput.value = '';
 	categorySelect.selectedIndex = 0;
+};
+
+const createNewTransaction = () => {
+	const newTransaction = document.createElement('div');
+	newTransaction.classList.add('transaction');
+	newTransaction.setAttribute('id', ID);
+	checkCategory(selectedCategory);
+
+	newTransaction.innerHTML = `
+        <p class="transaction-name">
+        ${categoryIcon} ${nameInput.value}
+        </p>
+        <p class="transaction-amount">
+        ${amountInput.value}zł 
+        <button class="delete" onclick="deleteTransatcion(${ID})"><i class="fas fa-times"></i></button>
+        </p>
+    `;
+
+	amountInput.value > 0
+		? incomeSection.appendChild(newTransaction) &&
+		  newTransaction.classList.add('income')
+		: expensesSection.appendChild(newTransaction) &&
+		  newTransaction.classList.add('expense');
+	moneyArr.push(parseFloat(amountInput.value));
+	countMoney(moneyArr);
+	closePanel();
+	ID++;
+	clearInputs();
+};
+
+const selectCategory = () => {
+	selectedCategory = categorySelect.options[categorySelect.selectedIndex].text;
+};
+
+const checkCategory = (transaction) => {
+	switch (transaction) {
+		case '[ + ] Przychód':
+			categoryIcon = '<i class="fas fa-money-bill-wave"></i>';
+			break;
+		case '[ - ] Zakupy':
+			categoryIcon = '<i class="fas fa-cart-arrow-down"></i>';
+			break;
+		case '[ - ] Jedzenie':
+			categoryIcon = '<i class="fas fa-hamburger"></i>';
+			break;
+		case '[ - ] Kino':
+			categoryIcon = '<i class="fas fa-film"></i>';
+			break;
+		// case '[ - ] Rozrywka':
+		// 	categoryIcon = '<i class="fas fa-film"></i>';
+		// 	break;
+		// case '[ - ] Oszczednosci':
+		// 	categoryIcon = '<i class="fas fa-film"></i>';
+		// 	break;
+	}
 };
 
 addTransactionBtn.addEventListener('click', showPanel);
